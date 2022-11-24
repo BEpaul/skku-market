bd19a1b31fafe14c0b8a885a510e79cf80f1b209
Minhyuk Kim <rhrlwld@gmail.com>
2022-11-24 21:29:02 +0900

    server init

---
+++ views/js/index.js
@@ -0,0 +1,37 @@
+$(function () {
+  console.log("hello");
+
+  $("#login_button").click(() => {
+    console.log("clicked!");
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:3000/",
+      data: "hellow",
+      dataType: "text",
+      success: (response) => {
+        console.log(response);
+      },
+      error: () => {
+        console.log("failed!");
+      },
+    });
+
+    location.href = "./list.html";
+  });
+
+  $("#signup_btn").click(() => {
+    console.log("signUp");
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:3000/login",
+      data: "asd",
+      dataType: "text",
+      success: (response) => {
+        console.log(response);
+      },
+      error: () => {
+        console.log("failed...");
+      },
+    });
+  });
+});
