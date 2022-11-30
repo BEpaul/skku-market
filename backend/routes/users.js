4d6819a52a7830476fde3caf4a722477655dab9a
JackCokebb <reo2726@gmail.com>
2022-11-30 17:13:09 +0900

    back

---
+++ backend/routes/users.js
@@ -0,0 +1,8 @@
+// ref: https://youngjinmo.github.io/2021/08/express-crud-rest-api/
+
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res, next)=>{
+    res.json()
+})
