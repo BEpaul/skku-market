334edcc8e57124b4f9869fe56ea427419113fd74
JackCokebb <reo2726@gmail.com>
2022-12-06 01:24:50 +0900

    new product page

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -5,7 +5,7 @@ const loadProductList =()=>{
 const renderProductCard =()=>{
     //https://stackoverflow.com/questions/4478863/show-image-from-blob-in-javascript
     const productImg = TODO; 
-
+    
     return `
     <div class="productCard">
         <img src="" alt="">
