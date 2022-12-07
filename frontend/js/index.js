c92947cfa323c07d8bc36fbaa7bb2e8d94f0c5c6
JackCokebb <reo2726@gmail.com>
2022-12-07 15:47:51 +0900

    list design

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,11 +1,21 @@
 const loadProductList =()=>{
 
+    //get product list from DB
+    const productList = getProductListFromDB();
+
+    //render product on the page for each
+    productList.forEach(element => {
+        renderProductCard(element);
+    });
+
 }
 
 const renderProductCard =()=>{
     //https://stackoverflow.com/questions/4478863/show-image-from-blob-in-javascript
     const productImg = TODO; 
+
     
+
     return `
     <div class="productCard">
         <img src="" alt="">
