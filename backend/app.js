01964e2540fe9d7f74de2a0b9f5756b9e5cd857c
Minhyuk Kim <rhrlwld@gmail.com>
2022-12-11 02:12:25 +0900

    feat: add userpost model, controller, route

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,13 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/user.routes");
 const postsRouter = require("./routes/post.routes");
 const commentsRouter = require("./routes/comment.routes");
+const userPostsRouter = require("./routes/userpost.routes");
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 app.use("/comments", commentsRouter);
+app.use("/userposts", userPostsRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
