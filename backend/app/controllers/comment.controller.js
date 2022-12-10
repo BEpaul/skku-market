c145151cbfb790ad4bfc294a78e46097a41f564d
JackCokebb <reo2726@gmail.com>
2022-12-10 22:16:29 +0900

    feat:login

--- backend/app/controllers/comment.controller.js
+++ backend/app/controllers/comment.controller.js
@@ -2,124 +2,180 @@ const Comment = require("../models/comments.model");
 const User = require("../models/users.model");
 
 // 새 객체 생성
-exports.create = (req,res)=>{
-    if(!req.body){
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-    };
-
-    const newComment = new Comment({
-        post_id: req.body.post_id,
-        user_id_from: req.body.user_id_from,
-        user_id_to: req.body.user_id_to,
-        comment: req.body.comment,
-        
-    });
-    //user existence test
-    //const from_user = User.findByID()
-    //const to_user = User.findByID()
-
-    // 데이터베이스에 저장
-    Comment.create(newComment, (err, data) =>{
-        if(err){
-            res.status(500).send({
-                message:
-                err.message || "Some error occured while creating the comment."
-            });
-        }
-        else{
-            res.status(201).send({message: "comment created", ...data})
-        }
-    })
-};
-
-// 전체 조회 
-exports.findAll = (req,res)=>{
-    Comment.getAll((err, data) => {
-        if (err)
-          res.status(500).send({
-            message:
-              err.message || "Some error occurred while retrieving comments."
-          });
-        else res.send(data);
-      });
-};
-
-// id로 조회
-exports.findOne = (req,res)=>{
-    Comment.findByID(req.params.commentId, (err, data) => {
-        if (err) {
-          if (err.kind === "not_found") {
-            res.status(404).send({
-              message: `Not found comment with id ${req.params.commentId}.`
-            });
-          } else {
-            res.status(500).send({
-              message: "Error retrieving comment with id " + req.params.commentId
-            });
-          }
-        } else res.send(data);
-      });
-};
-
-// id로 갱신
-exports.update = (req,res)=>{
-    // Validate Request
+exports.create = (req, res) => {
   if (!req.body) {
     res.status(400).send({
-      message: "Content can not be empty!"
+      message: "Content can not be empty!",
     });
   }
 
-  Comment.updateById(
-    req.params.commentId,
-    new Post(req.body),
-    (err, data) => {
-      if (err) {
-        if (err.kind === "not_found") {
-          res.status(404).send({
-            message: `Not found post with id ${req.params.commentId}.`
-          });
-        } else {
-          res.status(500).send({
-            message: "Error updating post with id " + req.params.commentId
-          });
-        }
-      } else res.send(data);
+  const newComment = new Comment({
+    post_id: req.body.post_id,
+    user_id_from: req.body.user_id_from,
+    user_id_to: req.body.user_id_to,
+    comment: req.body.comment,
+  });
+  //user existence test
+  //const from_user = User.findByID()
+  //const to_user = User.findByID()
+
+  // 데이터베이스에 저장
+  Comment.create(newComment, (err, data) => {
+    if (err) {
+      res.status(500).send({
+        message:
+          err.message || "Some error occured while creating the comment.",
+      });
+    } else {
+      res.status(201).send({ message: "comment created", ...data });
     }
-  );
+  });
+};
+
+// 전체 조회
+exports.findComments = (req, res) => {
+  const user_id = req.query.user_id;
+  const post_id = req.query.post_id;
+
+  if(user_id){
+    const comments_user_id = this.getByUserId(user_id);
+    if(!comments_user_id){
+      res.send([]);
+    }
+    else if(comments_user_id.kind){
+      console.log(comments_user_id.message);
+      res.status(500).send({
+        message:
+          comments_user_id.message || "Some error occurred while retrieving comments.",
+      });
+    }
+    else res.send(comments_user_id);
+  }
+  else if(post_id){
+    console.log("POST POST POST!")
+    const comments_post_id = this.getByPostId(post_id);
+    if(!comments_post_id){
+      console.log("sending undefined res...")
+      res.send([]);
+    }
+    else if(comments_post_id.kind){
+      console.log(comments_post_id.message);
+      console.log("sending undefined err...")
+      res.status(500).send({
+        message:
+          comments_post_id.message || "Some error occurred while retrieving comments.",
+      });
+    }
+    else {
+      console.log("sending not empty res...")
+      res.send(comments_post_id);
+    }
+  }else{
+    Comment.getAll((err, data) => {
+      if (err){
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving comments.",
+        });
+      }
+      else res.send(data);
+    });
+  }
+};
+//user_id로 조회
+exports.getByUserId = (user_id) => {
+  Comment.findByUserId(user_id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        return err;
+      } else {
+        return err;
+      }
+    } else return data;
+  });
+};
+//post_id로 조회
+exports.getByPostId = (post_id) => {
+  Comment.findByPostId(post_id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        return err;
+      } else {
+        return err;
+      }
+    } else return data;
+  });
+};
+
+
+
+// id로 조회
+exports.findOne = (req, res) => {
+  Comment.findByID(req.params.commentId, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found comment with id ${req.params.commentId}.`,
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving comment with id " + req.params.commentId,
+        });
+      }
+    } else res.send(data);
+  });
+};
+
+
+// id로 갱신
+exports.update = (req, res) => {
+  // Validate Request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+  }
+
+  Comment.updateById(req.params.commentId, new Post(req.body), (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found post with id ${req.params.commentId}.`,
+        });
+      } else {
+        res.status(500).send({
+          message: "Error updating post with id " + req.params.commentId,
+        });
+      }
+    } else res.send(data);
+  });
 };
 
 // id로 삭제
-exports.delete = (req,res)=>{
-    Comment.remove(req.params.commentId, (err, data) => {
-        if (err) {
-          if (err.kind === "not_found") {
-            res.status(404).send({
-              message: `Not found post with id ${req.params.commentId}.`
-            });
-          } else {
-            res.status(500).send({
-              message: "Could not delete post with id " + req.params.commentId
-            });
-          }
-        } else res.send({ message: `the comment was deleted successfully!` });
-      });
+exports.delete = (req, res) => {
+  Comment.remove(req.params.commentId, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found post with id ${req.params.commentId}.`,
+        });
+      } else {
+        res.status(500).send({
+          message: "Could not delete post with id " + req.params.commentId,
+        });
+      }
+    } else res.send({ message: `the comment was deleted successfully!` });
+  });
 };
 
 // 전체 삭제
-exports.deleteAll = (req,res)=>{
-    Comment.removeAll((err, data) => {
-        if (err)
-          res.status(500).send({
-            message:
-              err.message || "Some error occurred while removing all comments."
-          });
-        else res.send({ message: `All comments were deleted successfully!` });
+exports.deleteAll = (req, res) => {
+  Comment.removeAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all comments.",
       });
+    else res.send({ message: `All comments were deleted successfully!` });
+  });
 };
- 
-
- 
-
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,30 +1,79 @@
-const loadProductList =()=>{
+const checkLogin = () =>{
+    const user_id = sessionStorage.getItem("user_id");
+    if(user_id){
+        $("#headerBtn-login").css("display", "none");
+        $("#headerBtn-logout").css("display", "flex");
+        $("#userInfoText").html(sessionStorage.getItem("nickname"));
 
-    //get product list from DB
-    const productList = getProductListFromDB();
-
-    //render product on the page for each
-    productList.forEach(element => {
-        renderProductCard(element);
-    });
-
+    }
+    else{
+        $("#headerBtn-login").css("display", "flex");
+        $("#headerBtn-logout").css("display", "none");
+    }
 }
-const getProductListFromDB =()=>{
-    
+
+const logout = () =>{
+    sessionStorage.removeItem("user_id")
+    sessionStorage.removeItem("nickname")
+    location.href = "." 
 }
 
-const renderProductCard =()=>{
-    //https://stackoverflow.com/questions/4478863/show-image-from-blob-in-javascript
-    const productImg = TODO; 
+const loadProductList = async () => {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:3000/posts/",
+    //data: "hellow",
+    dataType: "json",
+    success: (response) => {
+      //console.log(response);
+      if(response.length>0){
+        const productList_div = document.querySelector("#productList");
+        productList_div.innerHTML = "";
+      }
+      
+      console.log("product list : " + response);
+      response.forEach((post) => {
+        console.log("post each : " + post.title);
+        $.ajax({
+          type: "GET",
+          url: `http://localhost:3000/comments?post_id=${post.post_id}`,
+          //data: "hellow",
+          dataType: "json",
+          success: (response) => {
+            console.log(response);
+            renderProductCard(post, response);
+          },
+          error: (err) => {
+            console.log("Get comment list failed! " + err);
+            alert("Get comment list failed!");
+          },
+        });
+      });
+    },
+    error: () => {
+      console.log("Get product list failed!");
+      alert("Get product list failed!");
+    },
+  });
+};
 
-    
+const getCommentsFromDB = async (post_id) => {};
 
-    return `
-    <div class="productCard">
-        <img src="" alt="">
-        <div class="cardTitle">card title</div>
-        <div class="price">1000원</div>
-        <div class="commentCount">댓글 수 : 0</div>
+const renderProductCard = (post, comments) => {
+  //https://stackoverflow.com/questions/4478863/show-image-from-blob-in-javascript
+  const productList_div = document.querySelector("#productList");
+  productList_div.innerHTML = `
+    <div class="productCard" id="${post.post_id}">
+        <img src="${post.image}" alt="product image">
+        <div class="cardTitle">${post.description}</div>
+        <div class="price">${post.price}원</div>
+        <div class="commentCount">댓글 수 : ${comments.length}</div>
     </div>
     `;
-}
+  return;
+};
+
+$(document).ready(function () {
+    checkLogin();
+    loadProductList();
+});
